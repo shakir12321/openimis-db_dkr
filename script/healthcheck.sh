9f5a78293ff4e580c3b04409b89a202d45316a1f
Patrick Delcroix <patrick.delcroix@swisstph.ch>
2022-05-10 17:54:33 +0200

    inverse logic

--- script/healthcheck.sh
+++ script/healthcheck.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 data=$(/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $SA_PASSWORD -Q "SELECT COUNT(*)  FROM master.dbo.sysdatabases WHERE name = N'$DB_NAME'" | tr -dc '0-9'| cut -c1 )
-if [ ${data} -eq "0" ]; then
-    exit 1
-else
+if [ ${data} -eq "1" ]; then
     exit 0
+else
+    exit 1
 fi
